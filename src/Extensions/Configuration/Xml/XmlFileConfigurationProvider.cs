b3856289da04bb5ca4c2cd64f19ad39647bb4ee0
mastolze <marco.stolze@karlstorz.com>
2021-02-03 18:37:56 +0100

    Adds XmlFile configuration provider.

--- src/Extensions/Configuration/Xml/XmlFileConfigurationProvider.cs
+++ src/Extensions/Configuration/Xml/XmlFileConfigurationProvider.cs
@@ -1,35 +1,35 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using MS.Extensions.IO.Json;
+using MS.Extensions.IO.Xml;
 
-namespace MS.Extensions.Configuration.Json
+namespace MS.Extensions.Configuration.Xml
 {
     /// <summary>
-    /// A JSON file based <see cref="FileConfigurationProvider"/>.
+    /// A XML file based <see cref="FileConfigurationProvider"/>.
     /// </summary>
-    public class JsonFileConfigurationProvider : FileConfigurationProvider
+    public class XmlFileConfigurationProvider : FileConfigurationProvider
     {
-        private readonly JsonFileConfigurationSource _source;
+        private readonly XmlFileConfigurationSource _source;
 
         /// <summary>
         /// Initializes a new instance with the specified source.
         /// </summary>
         /// <param name="source">The source settings.</param>
-        public JsonFileConfigurationProvider(JsonFileConfigurationSource source) : base(source)
+        public XmlFileConfigurationProvider(XmlFileConfigurationSource source) : base(source)
         {
             _source = source ?? throw new ArgumentNullException(nameof(source));
         }
 
         /// <summary>
-        /// Loads the JSON data from a stream.
+        /// Loads the XML data from a stream.
         /// </summary>
         /// <param name="stream">The stream to read.</param>
         public override void Load(Stream stream)
         {
             try
             {
-                Data = JsonStreamToDictionaryParser.ParseStream(
+                Data = XmlStreamToDictionaryParser.ParseStream(
                         stream, 
                         new IO.ParseToDictionaryOptions
                         {
@@ -41,7 +41,7 @@ namespace MS.Extensions.Configuration.Json
             }
             catch (Exception e)
             {
-                throw new FormatException("Could not parse the JSON file.", e);
+                throw new FormatException("Could not parse the XML file.", e);
             }
         }
     }
---
+++ src/Extensions/Configuration/Xml/XmlFileConfigurationSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace MS.Extensions.Configuration.Xml
+{
+    /// <summary>
+    /// Represents a XML file as an <see cref="IConfigurationSource"/>.
+    /// </summary>
+    public class XmlFileConfigurationSource : FileConfigurationSource
+    {
+        public IEnumerable<string> Parents { get; set; }
+
+        public Func<string, IEnumerable<string>, bool> IsIdentifier { get; set; } = (name, _) =>
+            string.Equals("name", name, StringComparison.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Builds the <see cref="XmlFileConfigurationProvider"/> for this source.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+        /// <returns>A <see cref="XmlFileConfigurationProvider"/></returns>
+        public override IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            EnsureDefaults(builder);
+            return new XmlFileConfigurationProvider(this);
+        }
+    }
+}
